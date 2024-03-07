2bacb5cb85af37557e88d284d4474f98e468e77b
yesiamanerd@gmail.com <yesiamanerd@gmail.com>
2024-03-07 22:24:23 +0000

    Adding new notebooks

---
+++ My Folder/(Clone) Notebook Basics.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# Demo Notebook
+
+# Cluster: My Demo Cluster
+
+print("Hello World!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello world from SQL!"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Title 1
+# MAGIC ## Title 2
+# MAGIC ### Title 3
+
+# COMMAND ----------
+
+# MAGIC %run ./includes/Setup
+
+# COMMAND ----------
+
+print(full_name)
+
+# COMMAND ----------
+
+# MAGIC %fs ls '/databricks-datasets'
+
+# COMMAND ----------
+
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+files = dbutils.fs.ls('/databricks-datasets')
+display(files)
